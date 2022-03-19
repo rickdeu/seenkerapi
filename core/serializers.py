3cdafedec642663e8604d13fe0bff236fcee8510
rickdeu <hangaloandre@gmail.com>
2022-03-19 16:59:08 +0100

    update store close and open

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import *
-
+import datetime
+import time
+#from datetime import datetime
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
@@ -25,5 +27,17 @@ class StoreSerializer(serializers.ModelSerializer):
         fields = ['id','name','phone','email', 'description', 'address', 'lat', 'log', 'image', 'open', 'close', 'created_at', 'isOpen']
     
     def get_isOpen(self, obj):
-        isOpen = 1
+        s1 = f"{obj.open}" #.strftime("%H:%M:%S")
+        s2 = f"{obj.close}"#.strftime("%H:%M:%S")
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        s3 = f"{current_time}"
+        d1 = datetime.datetime.strptime(s1, '%H:%M:%S')
+        d2 = datetime.datetime.strptime(s2, '%H:%M:%S')
+        d3 = datetime.datetime.strptime(s3,'%H:%M:%S')
+        if(d1<=d3 and d2>=d3):
+            isOpen = 1
+        else:
+            isOpen = 0
         return isOpen
+
