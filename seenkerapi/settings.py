bd3fd578928863321a2d9a95906a394d77c025d1
rickdeu <hangaloandre@gmail.com>
2022-03-19 15:56:10 +0100

    update settings

--- seenkerapi/settings.py
+++ seenkerapi/settings.py
@@ -123,8 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-"""STATICFILES_DIRS = (Path(BASE_DIR, "static"),)
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = (Path(BASE_DIR, "static"),)
+#MEDIA_ROOT=Path(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
+
+
+"""
+STATICFILES_DIRS = (Path(BASE_DIR, "static"),)
 MEDIA_ROOT=Path(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 STATIC_ROOT = Path(BASE_DIR, 'staticfiles')
